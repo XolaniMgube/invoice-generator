dc37704d4ff47f96248fa2d8e4ba8f626b4662e5
xolanim <mgubexn@telkom.co.za>
2023-12-15 16:34:48 +0200

    fixed sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,24 +1,43 @@
 export default function Sidebar() {
   return (
     <div className="w-3/12 bg-darkGrey h-screen text-sideBarText">
-      <div className="py-3 ">
-        <h3 className="px-2">Invoice Calculator</h3>
+      <div className="py-3 flex justify-center border-b">
+        <h3 className="text-lg">Invoice Calculator</h3>
       </div>
-      <div className="flex items-center my-2 px-4">
-        <img
-          width={30}
-          src="./profile-picture2.png"
-          alt="profile picture"
-        />
-        <div className="flex items-center">
-          <p className="">Xolani Mgube</p>
+      <div className="flex ps-2 py-3 my-auto">
+        <div className="my-auto">
+          <img width={25} src="./profile.png" alt="profile" />
+        </div>
+        <div className="ps-2">
+          <p className="my-auto">Xolani Mgube</p>
         </div>
       </div>
-      <div className="mt-10">
-        <ul className="p-0 m-0">
-          <li className="py-3 bg-active"><a className="px-2 text-darkGrey no-underline">Generate Invoice</a></li>
-          <li className="py-3"><a className="px-2 no-underline text-sideBarText">Update Invoice</a></li>
-          <li className="py-3"><a className="px-2 no-underline text-sideBarText">Settings</a></li>
+      <div className="">
+        <ul className="sidebar-list p-0 m-0">
+          <li className="py-3 bg-active">
+            <a
+              href="*"
+              className="ps-2 pe-28 py-3 hover:text-darkGrey text-darkGrey no-underline w-100"
+            >
+              Generate Invoice
+            </a>
+          </li>
+          <li className="py-3 hover:bg-active">
+            <a
+              href="*"
+              className="ps-2 pe-28 py-3 hover:text-darkGrey no-underline text-sideBarText w-96"
+            >
+              Update Invoice
+            </a>
+          </li>
+          <li className="py-3 hover:bg-active">
+            <a
+              href="*"
+              className="ps-2 pe-28 py-3 hover:text-darkGrey no-underline text-sideBarText"
+            >
+              Settings
+            </a>
+          </li>
         </ul>
       </div>
     </div>
